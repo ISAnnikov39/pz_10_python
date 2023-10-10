b18aacd09fe8b327ab274769e673e734d638297d
Konstantin <pkv39konstantin@yandex.ru>
2023-10-10 17:02:43 +0300

    correct

--- task44.py
+++ task44.py
@@ -4,13 +4,11 @@
 # Сможете ли вы это сделать без get_dummies?
 import pandas as pd 
 import random
-
 lst = ['robot'] * 10
 lst += ['human'] * 10
 random.shuffle(lst)
 data = pd.DataFrame({'whoAmI': lst})
 data.head()
-
 data.loc[data['whoAmI'] == 'human', 'human'] = '1'
 data.loc[data['whoAmI'] != 'human', 'human'] = '0'
 data.drop(columns='whoAmI')
